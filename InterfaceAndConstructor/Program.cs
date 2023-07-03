cd6d46d3c4141b9280c43a213b007e3022661321
Subhash Barela <barelasubhash16@gmail.com>
2023-07-03 14:51:29 +0530

    Create interface in C_Sharp

--- InterfaceAndConstructor/Program.cs
+++ InterfaceAndConstructor/Program.cs
@@ -10,42 +10,70 @@ namespace InterfaceAndConstructor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("1: Default and Para Constructor  \n2: CoprConstructor \n3: Static Constructor");
+
+            Console.WriteLine("1: Constructor  \n2: Interface \n3: Static Constructor");
             Console.WriteLine("Enter your choice");
             int choice = int.Parse(Console.ReadLine());
             switch (choice)
             {
                 case 1:
                     {
-                        ConstructorClass c_One = new ConstructorClass();
-                        ConstructorClass c_Two = new ConstructorClass(101, "Sagar");
+                        Console.WriteLine("1: Default and Para Constructor  \n2: CoprConstructor \n3: Static Constructor \n4: Private Constructor");
+                        Console.WriteLine("Enter Option that you want to exe");
+                        int opt = int.Parse(Console.ReadLine());
+                        switch (opt)
+                        {
+                            case 1:
+                                {
+                                    ConstructorClass c_One = new ConstructorClass();
+                                    ConstructorClass c_Two = new ConstructorClass(101, "Sagar");
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    CopyConstructorP obj1 = new CopyConstructorP(1, "Bridgelabs");
+                                    obj1.GetData();
+                                    CopyConstructorP obj2 = new CopyConstructorP(obj1);
+                                    obj2.GetData();
+                                    break;
+                                }
+                            case 3:
+                                {
+                                    StaticConstructor std = new StaticConstructor();
+                                    std.GetDetails();
+                                    break;
+                                }
+                            case 4:
+                                {
+                                    // PrivateConstructor prt=new PrivateConstructor("subhash","barela"); //in case of parameterize constructor
+                                    //PrivateConstructor prt1 = new PrivateConstructor(); //in case of non parameterize constructor we cannot create obj
+                                    PrivateConstructor.a = 10;
+                                    Console.WriteLine(PrivateConstructor.GetData());
+                                    break;
+                                }
+                            default:
+                                Console.WriteLine("Please insert valid option");
+                                break;
+                        }
                         break;
+
                     }
                 case 2:
                     {
-                        CopyConstructorP obj1 = new CopyConstructorP(1, "Bridgelabs");
-                        obj1.GetData();
-                        CopyConstructorP obj2 = new CopyConstructorP(obj1);
-                        obj2.GetData();
-                        break;
-                    }
-                case 3:
-                    {
-                        StaticConstructor std=new StaticConstructor();
-                        std.GetDetails();
-                        break;
-                    }
-                case 4:
-                    {
-                        // PrivateConstructor prt=new PrivateConstructor("subhash","barela"); //in case of parameterize constructor
-                       //PrivateConstructor prt1 = new PrivateConstructor(); //in case of non parameterize constructor we cannot create obj
-                        PrivateConstructor.a = 10;
-                       Console.WriteLine(PrivateConstructor.GetData());
+                        Shape s;
+                        s = new Rectangle();
+                        s.draw();
+                        s = new Circle();
+                        s.draw();
+                        s = new Triangle();
+                        s.draw();
                         break;
                     }
                 default:
-                    Console.WriteLine("Please insert valid option");
-                    break;
+                    {
+                        Console.WriteLine("Please Provide valid input..!");
+                        break;
+                    }
             }
             Console.ReadLine();
         }
---
+++ InterfaceAndConstructor/Shape.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAndConstructor
+{
+    public interface Shape
+    {
+
+        void draw();
+    }
+    public class Rectangle : Shape
+    {
+        public void draw()
+        {
+            Console.WriteLine("drawing rectangle...");
+        }
+    }
+    public class Circle : Shape
+    {
+        public void draw()
+        {
+            Console.WriteLine("drawing circle...");
+        }
+    }
+    public class Triangle : Shape
+    {
+        public void draw()
+        {
+            Console.WriteLine("drawing Triangle...");
+        }
+    }
+
+}
